4cd76635163887bad68f380cfa19f9f75840043f
OpenShift guest <ouk369@naver.com>
2014-04-01 02:12:52 -0400

    Led Pis

---
+++ arduino1.ino
@@ -0,0 +1,31 @@
+
+int led1 = 4;
+int led2 = 7;
+int led3 = 12;
+
+void setup() {                
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led1, HIGH);   
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  delay(1000);
+  digitalWrite(led1, HIGH);   
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);  
+  delay(1000);
+  digitalWrite(led1, LOW);       
+  digitalWrite(led2, HIGH);  
+  digitalWrite(led3, LOW);    
+   delay(1000) ;        
+  digitalWrite(led3, HIGH);  
+  digitalWrite(led2, LOW);    
+  digitalWrite(led1, LOW);           
+   delay(1000) ;
+ 
+}
+a
